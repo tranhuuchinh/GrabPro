9bb99c16b2bf2c0611f355a2b4c2c94c6c6b16bd
Andy Koi <anhkhoi1192002@gmail.com>
2023-07-02 18:54:26 +0700

    Khoi: Profile Page

--- GrabPro/src/routes/index.js
+++ GrabPro/src/routes/index.js
@@ -5,6 +5,7 @@ import PersonalScreen from "../pages/PersonalScreen/PersonalScreen";
 import PaymentPerson from "../pages/PaymentPerson/PaymentPerson";
 import AwardPerson from "../pages/AwardPerson/AwardPerson";
 import FavoritePerson from "../pages/FavoritePerson/FavoritePerson";
+import ProfilePerson from "../pages/ProfilePerson/ProfilePerson";
 
 const AppRouter = () => {
   return (
@@ -15,6 +16,7 @@ const AppRouter = () => {
         <Route path="/paymentperson" element={<PaymentPerson />} />
         <Route path="/awardperson" element={<AwardPerson />} />
         <Route path="/favorite" element={<FavoritePerson />} />
+        <Route path="/profile" element={<ProfilePerson />} />
       </Routes>
     </NativeRouter>
   );
