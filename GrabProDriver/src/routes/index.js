09af5b7f895be0d7030c6879f5df04d9d3c91216
tranhuuchinh0902 <tranhuuchinh500@gmail.com>
2023-07-10 03:32:40 +0700

    init driver

---
+++ GrabProDriver/src/routes/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { NativeRouter, Routes, Route } from "react-router-native";
+import HomeScreen from "../pages/HomeScreen/HomeScreen";
+
+const AppRouter = () => {
+  return (
+    <NativeRouter>
+      <Routes>
+        <Route path="/" element={<HomeScreen />} />
+      </Routes>
+    </NativeRouter>
+  );
+};
+
+export default AppRouter;
